280c0e3fdc55478157286c15feda6670fd0af0b0
rohitja26 <jainrohit0002@gamil.com>
2024-07-28 22:54:57 +0530

    feat/Backend-added

--- config/middlewares.js
+++ config/middlewares.js
@@ -9,4 +9,16 @@ module.exports = [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  settings: {
+    cors: {
+      enabled: true,
+      origin: ['http://localhost:5173'],
+      headers: [
+        'Content-Type',
+        'Authorization',
+        'X-Requested-With',
+        'Access-Control-Allow-Origin'
+      ],
+    },
+  },
 ];
