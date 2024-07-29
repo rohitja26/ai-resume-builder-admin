54cf65d1679613a40ef9706ec4f25327726a4f0c
rohitja26 <jainrohit0002@gamil.com>
2024-07-29 15:51:06 +0530

    feat/Backend-updated

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,14 +1,4 @@
-module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+module.exports = {
   settings: {
     cors: {
       enabled: true,
@@ -24,4 +14,15 @@ module.exports = [
       ],
     },
   },
-];
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+
+};
